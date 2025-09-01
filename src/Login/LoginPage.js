6e24ecef0cf5145e78aab8139c4f87dbdfde9ce2
Koisad <patrykpaczkow@gmail.com>
2025-09-01 19:09:27 +0200

    Added logo and title

--- src/Login/LoginPage.js
+++ src/Login/LoginPage.js
@@ -1,30 +1,28 @@
-// src/components/LoginPage.js
-
 import styled from 'styled-components';
 import GlobalStyles from '../components/GlobalStyles';
-import AuthPanel from './AuthPanel'; // ZMIANA: Z InputPanel na AuthPanel
+import AuthPanel from './AuthPanel';
 
 const AppContainer = styled.div`
-  display: flex;
-  width: 1200px;
-  height: 700px;
-  background-color: #1A1A1D;
-  border-radius: 15px;
-  overflow: hidden;
-  box-shadow: 0 10px 30px rgba(0, 0, 0, 0.5);
-  align-items: center;
-  justify-content: center;
+    display: flex;
+    width: 1200px;
+    height: 700px;
+    background-color: #1A1A1D;
+    border-radius: 15px;
+    overflow: hidden;
+    box-shadow: 0 10px 30px rgba(0, 0, 0, 0.5);
+    align-items: center;
+    justify-content: center;
 `;
 
 function LoginPage() {
-  return (
-    <>
-      <GlobalStyles/>
-      <AppContainer>
-        <AuthPanel /> {/* ZMIANA: Z InputPanel na AuthPanel */}
-      </AppContainer>
-    </>
-  );
+    return (
+        <>
+            <GlobalStyles/>
+            <AppContainer>
+                <AuthPanel /> 
+            </AppContainer>
+        </>
+    );
 }
 
 export default LoginPage;
